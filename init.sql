495e0300e26a3df66535e539879ebd7445027714
Student: Lamya Mamdouh Sayed Hussein <DPQUAI250123@mcicld.int>
2026-04-08 18:02:13 +0200

    Add Tea to task list

--- init.sql
+++ init.sql
@@ -10,4 +10,5 @@ INSERT INTO tasks VALUES
   (2, 'Eggs', 'done'),
   (3, 'Bread', 'pending'),
   (4, 'Butter', 'pending'),
-  (5, 'Orange juice', 'pending');
+  (5, 'Orange juice', 'pending'),
+  (6, 'Tea', 'pending');
